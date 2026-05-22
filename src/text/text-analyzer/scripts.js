167ab4ef2ee427d9f355461090b6aec7763eaf84
Adam Cyberpack <adam.lechowicz@cyberpack.pl>
2026-05-22 14:25:54 +0200

    code corrections

--- src/text/text-analyzer/scripts.js
+++ src/text/text-analyzer/scripts.js
@@ -1,52 +1,57 @@
+(() => {
+  document.querySelectorAll('.js-text-analyzer').forEach(textAnalyzer => {
+    const input = textAnalyzer.querySelector('.js-textarea');
+    const counterLetters = textAnalyzer.querySelector('.js-counter-letters');
+    const counterLettersNoSpace = textAnalyzer.querySelector('.js-counter-letters-no-space');
+    const counterWords = textAnalyzer.querySelector('.js-counter-words');
+    const counterSentences = textAnalyzer.querySelector('.js-counter-sentences');
+    const counterStandardTextPageCount = textAnalyzer.querySelector('.js-counter-standerd-text-page');
+    const counterReadTime = textAnalyzer.querySelector('.js-counter-read-time');
+    const stats = {};
 
-document.querySelectorAll('.js-text-analyzer').forEach(textAnalyzer => {
-  const input = textAnalyzer.querySelector('.js-textarea');
-  const counterLetters = textAnalyzer.querySelector('.js-counter-letters');
-  const counterLettersNoSpace = textAnalyzer.querySelector('.js-counter-letters-no-space');
-  const counterWords = textAnalyzer.querySelector('.js-counter-words');
-  const counterSentences = textAnalyzer.querySelector('.js-counter-sentences');
-  const counterStandardTextPage = textAnalyzer.querySelector('.js-counter-standerd-text-page');
-  const counterReadTime = textAnalyzer.querySelector('.js-counter-read-time');
-  const stats = {};
+    input.addEventListener('input', (e) => {
+      gatherStats(e);
+      renderStats(stats);
+    });
 
-  input.addEventListener('input', (e) => {
-    gatherStats();
-    renderStats(stats);
-    function gatherStats() {
-      stats.lettersLength = e.target.value.length;
-      stats.lettersLengthNoSpace = e.target.value.split('').filter((value) => {
+    textAnalyzer.querySelector('.js-btn-eraser').addEventListener('click' , () => {
+      input.value = '';
+      input.dispatchEvent(new Event('input'));
+    });
+
+    textAnalyzer.querySelector('.js-btn-cleaner').addEventListener('click', () => {
+      input.value = input.value.replaceAll(/\s/g, ' ').trim();
+      input.value = input.value.replaceAll(/ + /g, ' ').trim();
+      input.dispatchEvent(new Event('input'));
+    });
+
+    function gatherStats(e) {
+      stats.chartsCount = e.target.value.length;
+      stats.chartsNoSpaceCount = e.target.value.split('').filter((value) => {
         return value != ' ';
-      });
+      }).length;
 
       // using regexp, all signs in [] will be replaced as ' ' (meaning space), then it create array that store all words separately, then we count it.
-      stats.wordLength = e.target.value.replaceAll(/[,/.?!+=""''_\-(){}:;><|[\]\\]/g, ' ').split(' ').filter((value) => {
+      stats.wordCount = e.target.value.replaceAll(/[,/.?!+=""''_\-(){}:;><|[\]\\@#&]/g, ' ').split(' ').filter((value) => {
         return value;
-      });
-      stats.sentenceLenght = e.target.value.replaceAll(/[?!]/g, '.').replaceAll(/\s/g,'').split('.').filter((value) => {
+      }).length;
+      stats.sentenceCount = e.target.value.replaceAll(/[?!]/g, '.').replaceAll(/\s/g,'').split('.').filter((value) => {
         return value != '';
-      });
-      stats.standardTextPage = Number.parseFloat(stats.lettersLength/1800).toFixed(2);
-      stats.readTime = `${Number.parseFloat(stats.wordLength.length/200).toFixed()} min`;
+      }).length;
+      stats.standardTextPageCount = Number.parseFloat(parseFloat(stats.chartsCount/1800).toFixed(2));
+      stats.readTimeCount = parseFloat((Math.floor(stats.wordCount/2.5)));
+      // stats.readTimeCount = `${Math.floor(readTimeRawCount / 60)}:${readTimeRawCount % 60}`;
+      // readTimeCount = `${Math.floor(readTimeCount / 60)}:${readTimeCount % 60}`;
+      console.log(stats)
     }
 
     function renderStats(stats) {
-      counterLetters.innerHTML = stats.lettersLength;
-      counterLettersNoSpace.innerHTML = stats.lettersLengthNoSpace.length;
-      counterWords.innerHTML = stats.wordLength.length;
-      counterSentences.innerHTML = stats.sentenceLenght.length;
-      counterStandardTextPage.innerHTML = stats.standardTextPage;
-      counterReadTime.innerHTML = stats.readTime;
+      counterLetters.innerHTML = stats.chartsCount;
+      counterLettersNoSpace.innerHTML = stats.chartsNoSpaceCount;
+      counterWords.innerHTML = stats.wordCount;
+      counterSentences.innerHTML = stats.sentenceCount;
+      counterStandardTextPageCount.innerHTML = stats.standardTextPageCount;
+      counterReadTime.innerHTML = `${Math.floor(stats.readTimeCount / 60)}:${stats.readTimeCount % 60}`;
     }
   });
-
-  textAnalyzer.querySelector('.js-btn-eraser').addEventListener('click' , () => {
-    input.value = '';
-    input.dispatchEvent(new Event('input'));
-  });
-
-  textAnalyzer.querySelector('.js-btn-cleaner').addEventListener('click', () => {
-    input.value = input.value.replaceAll(/[\s]/g, ' ').trim();
-    input.value = input.value.replaceAll(/ + /g, ' ').trim();
-    input.dispatchEvent(new Event('input'));
-  });
-});
+})();
